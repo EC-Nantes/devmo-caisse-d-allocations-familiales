3b1f4c487abdd729bb4e88a56d51453b725125e7
haytambensribit <haytam.bensribit@gmail.com>
2026-03-12 08:33:48 +0100

    Configuration des couleurs

--- app/src/main/java/com/example/devmo_caisse_d_allocations_familiales/ui/theme/Color.kt
+++ app/src/main/java/com/example/devmo_caisse_d_allocations_familiales/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.example.devmo_caisse_d_allocations_familiales.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val CafPrimary = Color(0xFF0B9EC5)      // bleu bouton principal
+val CafTurquoise = Color(0xFF11C5BA)    // turquoise icônes / accents
+val CafTitle = Color(0xFF0C8F8A)        // vert foncé du nom
+val CafText = Color(0xFF1E2430)         // texte principal
+val CafSubText = Color(0xFF6E7685)      // texte secondaire
+val CafLightGray = Color(0xFFF3F3F6)    // carte claire
+val CafBorder = Color(0xFFE3E4E8)       // bordures
+val CafNavGray = Color(0xFF98A1B2)      // icônes non actives
+val CafWhite = Color(0xFFFFFFFF)
